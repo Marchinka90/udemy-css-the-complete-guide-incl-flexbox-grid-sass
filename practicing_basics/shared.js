9817cbce0f7f2c934bf1c16fc9899d622e8c3a6d
Marchinka90 <martin_marchev@abv.bg>
2023-10-13 15:30:13 +0300

    Added mobile nav to all pages

--- practicing_basics/shared.js
+++ practicing_basics/shared.js
@@ -2,19 +2,40 @@ let backdrop = document.querySelector('.backdrop');
 let modal = document.querySelector('.modal');
 let modalNoButton = document.querySelector('.modal__action--negative');
 let selectPlanButton = document.querySelectorAll('.plan button');
+let toogleButton = document.querySelector('.toggle-button');
+let mobileNav = document.querySelector('.mobile-nav');
 
 for (let i = 0; i < selectPlanButton.length; i++) {
     selectPlanButton[i].addEventListener('click', function () {
-        modal.style.display = 'block';
-        backdrop.style.display = 'block';
+        // modal.style.display = 'block';
+        // backdrop.style.display = 'block';
+        // modal.className = 'open'; // This will actually overwrite the complete class list
+        modal.classList.add('open');
+        backdrop.classList.add('open');
     });
 }
 
-backdrop.addEventListener('click', closeModal);
-
-modalNoButton.addEventListener('click', closeModal);
+backdrop.addEventListener('click', function() {
+    // mobileNav.style.display = 'none';
+    mobileNav.classList.remove('open');
+    closeModal();
+});
+if (modalNoButton) {
+    modalNoButton.addEventListener('click', closeModal);
+}
 
 function closeModal() {
-    modal.style.display = 'none';
-    backdrop.style.display = 'none';
+    // modal.style.display = 'none';
+    // backdrop.style.display = 'none';
+    if (modal) {
+        modal.classList.remove('open');
+    }
+    backdrop.classList.remove('open');
 }
+
+toogleButton.addEventListener('click', function() {
+    // mobileNav.style.display = 'block';
+    // backdrop.style.display = 'block';
+    mobileNav.classList.add('open');
+    backdrop.classList.add('open');
+});
